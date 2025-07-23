1198731e7da59470313f89d55e4de1d6c4f24e64
Den09hub <denyel.1005@gmail.com>
2025-07-23 17:05:17 -0400

    task2

--- app/index.tsx
+++ app/index.tsx
@@ -2,27 +2,47 @@ import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 flex-row flex-wrap p-20 space-y-4 bg-slate-500">
-      {/* <Text className="text-5xl text-dark-200 font-bold bg-slate-500 p-10">Bem vindo</Text> */}
-      <View className="w-1/2 p-2 bg-white">
-        {/* <Image source={require('./assets/ps5.jpg')}/> */}
-        <Text className="text-2xl font-bold">Nome</Text>
+    <View className="flex-1 bg-slate-300">
+
+      <View className="">
+        <Text className="text-2xl px-10 mt-10 font-bold">Best Dress Collection</Text>
       </View>
-      <View className="w-1/2 p-2 bg-white">
-        <Text className="text-2xl font-bold">Nome</Text>
+
+      <View className="flex-row justify-center gap-6">
+
+        <View className="flex-col gap-6">
+
+          <View className="w-72 h-52 p-2 bg-white rounded-xl shadow justify-between">
+            <View className="flex-1">
+              <Image source={require('../assets/images/ps5.jpg')} className="w-full h-32 rounded-xl" resizeMode="contain"/>
+            </View>
+            <Text className="text-2xl font-bold mt-2">Nome</Text>
+          </View>
+          <View className="w-72 h-52 p-2 bg-white rounded-xl shadow justify-between">
+            <Text className="text-2xl font-bold">Nome</Text>
+          </View>
+          <View className="w-72 h-52 p-2 bg-white rounded-xl shadow justify-between">
+            <Text className="text-2xl font-bold">Nome</Text>
+          </View>
+
+        </View>
+
+        <View className="flex-col gap-6">
+
+          <View className="w-72 h-52 p-2 bg-white rounded-xl shadow justify-between">
+            <Text className="text-2xl font-bold">Nome</Text>
+          </View>
+          <View className="w-72 h-52 p-2 bg-white rounded-xl shadow justify-between">
+            <Text className="text-2xl font-bold">Nome</Text>
+          </View>
+          <View className="w-72 h-52 p-2 bg-white rounded-xl shadow justify-between">
+            <Text className="text-2xl font-bold">Nome</Text>
+          </View>
+
+        </View>
+
       </View>
-      <View className="w-1/2 p-2 bg-white">
-        <Text className="text-2xl font-bold">Nome</Text>
-      </View>
-      <View className="w-1/2 p-2 bg-white">
-        <Text className="text-2xl font-bold">Nome</Text>
-      </View>
-      <View className="w-1/2 p-2 bg-white">
-        <Text className="text-2xl font-bold">Nome</Text>
-      </View>
-      <View className="w-1/2 p-2 bg-white">
-        <Text className="text-2xl font-bold">Nome</Text>
-      </View>
+
     </View>
   );
 }
